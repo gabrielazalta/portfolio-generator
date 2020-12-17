13e2aeb41241347333bebd5a0d03d0d887a6bdad
Gabriela Zalta <gzalta96@gmail.com>
2020-12-17 12:09:54 -0500

    included stylesheet

--- src/page-template.js
+++ src/page-template.js
@@ -1,19 +1,19 @@
 // create the about section
 const generateAbout = aboutText => {
     if (!aboutText) {
-        return '';
+      return '';
     }
-
+  
     return `
       <section class="my-3" id="about">
         <h2 class="text-dark bg-primary p-2 display-inline-block">About Me</h2>
         <p>${aboutText}</p>
       </section>
     `;
-};
-
-// create the projects section
-const generateProjects = projectsArr => {
+  };
+  
+  // create the projects section
+  const generateProjects = projectsArr => {
     return `
       <section class="my-3" id="portfolio">
         <h2 class="text-dark bg-primary p-2 display-inline-block">Work</h2>
@@ -21,68 +21,46 @@ const generateProjects = projectsArr => {
         ${projectsArr
           .filter(({ feature }) => feature)
           .map(({ name, description, languages, link }) => {
-            return ` <
-        div class = "col-12 mb-2 bg-dark text-light p-3" >
-        <
-        h3 class = "portfolio-item-title text-light" > $ {
-            name
-        } < /h3> <
-        h5 class = "portfolio-languages" >
-        Built With:
-        $ {
-            languages.map(language => language).join(',')
-        } <
-        /h5> <
-        p > $ {
-            description
-        } < /p> <
-        a href = "${link}"
-    class = "btn" > < i class = "fab fa-github mr-2" > < /i>View Project on GitHub</a >
-        <
-        /div>
-    `;
+            return `
+            <div class="col-12 mb-2 bg-dark text-light p-3">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                ${languages.map(language => language).join(',')}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+          `;
           })
           .join('')}
   
         ${projectsArr
           .filter(({ feature }) => !feature)
           .map(({ name, description, languages, link }) => {
-            return ` <
-    div class = "col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column" >
-        <
-        h3 class = "portfolio-item-title text-light" > $ {
-            name
-        } < /h3> <
-        h5 class = "portfolio-languages" >
-        Built With:
-        $ {
-            languages.join(', ')
-        } <
-        /h5> <
-        p > $ {
-            description
-        } < /p> <
-        a href = "${link}"
-    class = "btn mt-auto" > < i class = "fab fa-github mr-2" > < /i>View Project on GitHub</a >
-        <
-        /div>
-    `;
+            return `
+            <div class="col-12 col-md-6 mb-2 bg-dark text-light p-3 flex-column">
+              <h3 class="portfolio-item-title text-light">${name}</h3>
+              <h5 class="portfolio-languages">
+                Built With:
+                ${languages.join(', ')}
+              </h5>
+              <p>${description}</p>
+              <a href="${link}" class="btn mt-auto"><i class="fab fa-github mr-2"></i>View Project on GitHub</a>
+            </div>
+          `;
           })
           .join('')}
         </div>
       </section>
     `;
-};
-
-// export function to generate entire page
-module.exports = templateData => {
+  };
+  
+  // export function to generate entire page
+  module.exports = templateData => {
     // destructure page data by section
-    const {
-        projects,
-        about,
-        ...header
-    } = templateData;
-
+    const { projects, about, ...header } = templateData;
+  
     return `
     <!DOCTYPE html>
     <html lang="en">
@@ -117,4 +95,5 @@ module.exports = templateData => {
     </body>
     </html>
     `;
-};
+  };
+  
